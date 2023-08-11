19ba638ea2c293c154fb5ea19059c6139e5dd4e9
Suyogya Taneja <taneja.taneja@monash.edu>
2023-08-11 11:25:26 +1000

    Add support for subtraction

--- calc.py
+++ calc.py
@@ -5,6 +5,9 @@ def compute(expression):
     num1 = int(values[2])
     if operator == '+':
         return num0 + num1
+     return num0 + num1
+    elif operator == '-':
+        return num0 - num1
     else:
         print('unknown operator!')
         return None
